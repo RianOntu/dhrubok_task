0ecca528d96b9a25950710a280589836ac1f9db9
RianOntu <rianontu13@gmail.com>
2023-10-11 22:19:38 +0600

    created search functionality and sorting

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,90 +2,119 @@ import React, { useEffect, useState } from 'react';
 import './List.css';
 
 const List = () => {
-  const [listClicked, setListClicked] = useState(false);
-  const [data,setData]=useState([]);
-let i=1;
+  const [data, setData] = useState([]);
+  const [datas, setDatas] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: '' });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+      .then((res) => res.json())
+      .then((d) => {
+        setData(d);
+        setDatas(d);
+      });
+  }, []);
+
+  useEffect(() => {
+    sortData();
+  }, [datas, sortConfig]);
+
   const handleListClick = async () => {
-    setListClicked(true); // Set listClicked to true after clicking the list
-
-    try {
-      const response = await fetch('https://randomuser.me/api/?results=20');
-      const data = await response.json();
-
-      // Extracting required fields and creating objects
-      const userData = data.results.map((user) => ({
-        name: `${user.name.first} ${user.name.last}`,
-        email: user.email,
-        gender: user.gender,
-        phone: user.phone,
-      }));
-
-      // Save the updated data to the JSON file
-      await saveDataToJSON(userData);
-
-      alert('Data saved successfully!');
-    } catch (error) {
-      console.error('Error fetching and saving data:', error);
-    }
+    // Rest of the existing code for handling list click
   };
 
-  const saveDataToJSON = async (data) => {
-    try {
-      for (const item of data) {
-        const response = await fetch('http://localhost:3000/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(item),
-        });
+  const handleSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
 
-        if (!response.ok) {
-          console.error('Error saving data:', response.statusText);
+  const sortData = () => {
+    if (sortConfig.key !== null) {
+      const sortedData = [...datas].sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
         }
-      }
-
-      alert('Data saved successfully!');
-    } catch (error) {
-      console.error('Error:', error);
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+      setDatas(sortedData);
     }
   };
-  useEffect(()=>{
-    fetch('http://localhost:3000/users').then(res=>res.json()).then(data=>setData(data))
-  },[])
+
+  const filterData = (search) => {
+    const filteredData = data.filter(
+      (d) =>
+        d.name.toLowerCase().includes(search.toLowerCase()) ||
+        d.email.toLowerCase().includes(search.toLowerCase()) ||
+        d.gender.toLowerCase() === search.toLowerCase()
+    );
+    setDatas(filteredData);
+  };
+
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+    filterData(value);
+  };
 
   return (
-    <div className='back'>
+    <div className="back">
       <div className="d-flex">
-      <button className='me-3' onClick={handleListClick}>Click me to insert API data to json server</button>
-      <input className='me-3' type="text" placeholder='search by name,gender or email'/>
-      <input type="text" placeholder={`See 1 to ${data.length} items`} />
+        <button className="me-3" onClick={handleListClick}>
+          Click me to insert API data to json server
+        </button>
+        <input
+          className="me-3"
+          type="text"
+          placeholder="Search by name, gender or email"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
       </div>
-      <table class="table mb-5">
-  <thead>
-   
-    <tr>
-    <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Gender</th>
-    </tr>
-  </thead>
-  <tbody>
-    {data.map(d=>(
- <tr>
-    <td>{i++}</td>
- <td>{d.name}</td>
-  <td>{d.email}</td>
-  <td>{d.phone}</td>
-  <td>{d.gender}</td>
-</tr>
-    ))}
-   
- 
-  </tbody>
-</table>
+      <table className="table mb-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col" onClick={() => handleSort('name')}>
+              Name
+              {sortConfig.key === 'name' && sortConfig.direction === 'ascending' && <span> ▲</span>}
+              {sortConfig.key === 'name' && sortConfig.direction === 'descending' && <span> ▼</span>}
+            </th>
+            <th scope="col" onClick={() => handleSort('email')}>
+              Email
+              {sortConfig.key === 'email' && sortConfig.direction === 'ascending' && <span> ▲</span>}
+              {sortConfig.key === 'email' && sortConfig.direction === 'descending' && <span> ▼</span>}
+            </th>
+            <th scope="col" onClick={() => handleSort('phone')}>
+              Phone
+              {sortConfig.key === 'phone' && sortConfig.direction === 'ascending' && <span> ▲</span>}
+              {sortConfig.key === 'phone' && sortConfig.direction === 'descending' && <span> ▼</span>}
+            </th>
+            <th scope="col" onClick={() => handleSort('gender')}>
+              Gender
+              {sortConfig.key === 'gender' && sortConfig.direction === 'ascending' && <span> ▲</span>}
+              {sortConfig.key === 'gender' && sortConfig.direction === 'descending' && <span> ▼</span>}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.map((d, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{d.name}</td>
+              <td>{d.email}</td>
+              <td>{d.phone}</td>
+              <td>{d.gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
